055f74c653d7b0819c9397d48b22fdd235c07496
Federico <vulcanofederico94@gmail.com>
2025-07-31 11:47:36 +0200

    meal item card

---
+++ components/MealItem.tsx
@@ -0,0 +1,69 @@
+import { Image, Platform, Pressable, StyleSheet, Text, View } from "react-native";
+import Meal from "../models/meal";
+
+interface MealItemProps {
+	meal: Meal
+}
+
+function MealItem({ meal }: MealItemProps) {
+	return (
+		<View style={styles.mealItem}>
+			<Pressable style={({ pressed }) => pressed ? styles.buttonPressed : null}>
+				<View style={styles.innerContainer}>
+					<View>
+						<Image source={{ uri: meal.imageUrl }} style={styles.image} />
+						<Text style={styles.title}>{meal.title}</Text>
+					</View>
+					<View style={styles.details}>
+						<Text style={styles.detailItem}>{meal.duration}</Text>
+						<Text style={styles.detailItem}>{meal.complexity.toUpperCase()}</Text>
+						<Text style={styles.detailItem}>{meal.affordability.toUpperCase()}</Text>
+					</View>
+				</View>
+			</Pressable>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	mealItem: {
+		margin: 16,
+		borderRadius: 8,
+		overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+		backgroundColor: 'white',
+		shadowColor: 'black',
+		shadowOpacity: 0.35,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 8,
+	},
+	innerContainer: {
+		borderRadius: 8,
+		overflow: 'hidden'
+	},
+	image: {
+		width: '100%',
+		height: 200
+	},
+	title: {
+		fontWeight: 'bold',
+		textAlign: 'center',
+		fontSize: 18,
+		margin: 8
+	},
+	details: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+		margin: 8
+	},
+	detailItem: {
+		fontSize: 12,
+		fontWeight: '300'
+	},
+	buttonPressed: {
+		opacity: 0.5
+	},
+})
+
+export default MealItem;
--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,4 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, ListRenderItemInfo, StyleSheet, View } from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+import Meal from "../models/meal";
 import { MealsOverviewScreenNavigationProp, MealsOverviewScreenRouteProp } from "../types/navigation";
 
 interface MealsOverviewScreenProps {
@@ -11,10 +14,17 @@ function MealsOverviewScreen({ navigation, route }: MealsOverviewScreenProps) {
 	// Recupera il parametro categoryId dalla route
 	const categoryId = route.params.categoryId;
 
+	const displayedMeals = MEALS.filter((mealItem: Meal) => {
+		return mealItem.categoryIds.indexOf(categoryId) >= 0;
+	});
+
+	function renderMealItem(itemData: ListRenderItemInfo<Meal>) {
+		return (<MealItem meal={itemData.item} />)
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Meals Overview Screen</Text>
-			<Text>Category ID: {categoryId}</Text>
+			<FlatList data={displayedMeals} keyExtractor={(item: Meal) => item.id} renderItem={renderMealItem} />
 		</View>
 	);
 
